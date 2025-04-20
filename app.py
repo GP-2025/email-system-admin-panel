a984b97744e51e35e2cf866c3fc9c38a1d589bd8
Ismail Sherif <ismailsherifwork@gmail.com>
2025-04-20 13:17:16 +0200

    Update home route to render home.html instead of index.html

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
 @app.route('/')
-def index():
-    return flask.render_template('index.html')
+def home():
+    return flask.render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5080)
