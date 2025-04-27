7b2a9cd64470a3f369de1dd018429941e74ee8cb
Ismail Sherif <ismailsherifwork@gmail.com>
2025-04-27 23:14:06 +0300

    refactor: streamline modal handling in JavaScript and update admin templates to include partials for loader, alert templates, and headers for improved maintainability

--- static/js/modal_handler.js
+++ static/js/modal_handler.js
@@ -3,15 +3,6 @@ const addModalOpenButton = document.getElementById("add-modal-open-button");
 const addModalCloseButton = document.getElementById("add-modal-close-button");
 const addModalBackdrop = document.getElementById("add-modal");
 
-document.addEventListener('DOMContentLoaded', () => {
-    addModalOpenButton.addEventListener('click', () => {
-        addModalBackdrop.classList.remove('pointer-events-none', 'opacity-0');
-    });
-
-    addModalCloseButton.addEventListener('click', () => {
-        addModalBackdrop.classList.add('pointer-events-none', 'opacity-0');
-    });
-});
 
 function updateModalOpenButton(row_id) {
     const updateModalBackdrop = document.querySelector(`[data-modal-row-id="${row_id}"]`);
@@ -22,3 +13,17 @@ function updateModalCloseButton(row_id) {
     const updateModalBackdrop = document.querySelector(`[data-modal-row-id="${row_id}"]`);
     updateModalBackdrop.classList.add('pointer-events-none', 'opacity-0');
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (addModalOpenButton) {
+        addModalOpenButton.addEventListener('click', () => {
+            addModalBackdrop.classList.remove('pointer-events-none', 'opacity-0');
+        });
+    }
+    if (addModalCloseButton) {
+        addModalCloseButton.addEventListener('click', () => {
+            addModalBackdrop.classList.add('pointer-events-none', 'opacity-0');
+        });
+    }
+});
